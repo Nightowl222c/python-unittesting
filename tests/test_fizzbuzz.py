a822f9abeb9877e9e9e7bd5d9b7ba39257c228ba
Colin Zimmermann <colinzimmermann93@gmail.com>
2024-05-06 21:28:16 +0200

    Add changes fizzbuzz

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,9 +1,21 @@
-def fizzbuzz(number):
-    if number % 15 == 0:
-        return "fizz buzz"
-    elif number % 3 == 0:
-        return "fizz"
-    elif number % 5 == 0:
-        return "buzz"
-    else:
-        return number
+import unittest
+
+from fizzbuzz import fizzbuzz
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), "fizz")
+
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), "buzz")
+
+    def test_fizz_buzz(self):
+        self.assertEqual(fizzbuzz(15), "fizz buzz")
+
+    def test_neither(self):
+        self.assertEqual(fizzbuzz(1), 1)
+        self.assertEqual(fizzbuzz(2), 2)
+        self.assertEqual(fizzbuzz(4), 4)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
